0a578defe97b28b0fc1020421cf01c13bd64e4bc
Furkan Kaan Alkan <alkanfurkankaan@gmail.com>
2025-12-26 16:54:48 +0300

    User Interface has been done

--- obezite_kontrol.py
+++ obezite_kontrol.py
@@ -11,7 +11,7 @@ label_1 = tkinter.Label(my_window, text=" enter your weight (kg) : ", font=('bol
 label_1.pack()
 label_1.config(pady=20)
 
-entry1 = tkinter.Entry(my_window,width=22)
+entry1 = tkinter.Entry(width=22)
 entry1.pack()
 
 
@@ -20,7 +20,8 @@ label_2 = tkinter.Label(my_window, text=" enter your height (cm) : ", font=('bol
 label_2.config(pady=20)
 label_2.pack()
 
-entry2 = tkinter.Entry(my_window,width=22)
+
+entry2 = tkinter.Entry(width=22)
 entry2.pack()
 
 
@@ -29,16 +30,32 @@ def calculate():
         weight = float(entry1.get())
         height = float(entry2.get())
         a = weight / ((height/100) * (height/100))
-        print(a)
-        return a
-    except ValueError:
-        print('you should enter a intiger')
+
+
+        if  a < 18.5:
+            print("you are underweight")
+        elif 18.5 < a < 24.9:
+            print("you are normal")
+        elif 25 < a < 29.9:
+            print("you are overweight")
+        elif 30 < a < 34.9:
+            print("you are obese")
+        else:
+            print("you are extremly obese")
+
+        label_3 = tkinter.Label(my_window, text=f'--> {a} ', font=('bold', 10))
+        label_3.config(pady=20)
+        label_3.pack()
+
+
+    except :
+        label_3 = tkinter.Label(my_window, text='enter a valid number', font=('bold', 10))
+        label_3.config(pady=20)
+        label_3.pack()
+        return
 
 my_button = tkinter.Button(text="Calculate", command=calculate)
 my_button.pack()
-
-
-
 
 
 
@@ -50,3 +67,4 @@ my_button.pack()
 
 
 my_window.mainloop()
+
