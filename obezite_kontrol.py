cee3bf9a8bcae364c335acff00fe31c33bfd0aba
Furkan Kaan Alkan <alkanfurkankaan@gmail.com>
2025-12-26 22:37:52 +0300

    it done but I will try to make it better

--- obezite_kontrol.py
+++ obezite_kontrol.py
@@ -1,6 +1,5 @@
 import tkinter
-
-
+from time import process_time
 
 my_window = tkinter.Tk()
 my_window.title("BMI Calculator")
@@ -20,43 +19,41 @@ label_2 = tkinter.Label(my_window, text=" enter your height (cm) : ", font=('bol
 label_2.config(pady=20)
 label_2.pack()
 
-
 entry2 = tkinter.Entry(width=22)
 entry2.pack()
+
+label_3 = tkinter.Label(font=('bold', 10))
+label_3.pack()
 
 
 def calculate():
     try:
+        global label_3
+
         weight = float(entry1.get())
         height = float(entry2.get())
         a = weight / ((height/100) * (height/100))
-
+        label_3.config(text='--> ', pady=20)
 
         if  a < 18.5:
-            print("you are underweight")
+            label_3.config(text=f" {a:.2f} you are underweight")
         elif 18.5 < a < 24.9:
-            print("you are normal")
+            label_3.config(text=f" {a:.2f} you are normal")
         elif 25 < a < 29.9:
-            print("you are overweight")
+            label_3.config(text=f" {a:.2f} you are overweight")
         elif 30 < a < 34.9:
-            print("you are obese")
+            label_3.config(text=f" {a:.2f} you are obese")
         else:
-            print("you are extremly obese")
-
-        label_3 = tkinter.Label(my_window, text=f'--> {a} ', font=('bold', 10))
-        label_3.config(pady=20)
-        label_3.pack()
-
+            label_3.config(text=f" {a:.2f} you are extremly obese")
 
     except :
-        label_3 = tkinter.Label(my_window, text='enter a valid number', font=('bold', 10))
-        label_3.config(pady=20)
-        label_3.pack()
-        return
+        label_3.config(text='enter a valid number')
 
-my_button = tkinter.Button(text="Calculate", command=calculate)
+
+
+my_button = tkinter.Button(text="Calculate", command=calculate )
 my_button.pack()
-
+label_3.config(text='enter a valid number')
 
 
 
